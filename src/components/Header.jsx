37f2a83470d50ac8dba37ac235cfc5d408723380
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2026-01-20 22:52:11 +0000

    Create header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header>
+            <img src="/src/assets/book-icon.png" alt="book icon" />
+            <h1>2026 Reading List</h1>
+        </header>
+    )
+}
