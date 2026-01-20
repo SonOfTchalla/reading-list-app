7c978029944a372a3666e4fe50aa80ffad9d2856
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2026-01-20 23:48:00 +0000

    Create sampleData for mock database

---
+++ src/sampleData.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        "id":0,
+        "img": {
+            "src": "https://m.media-amazon.com/images/I/81C7BR23joL._AC_UF894,1000_QL80_.jpg",
+            "alt": "The Hedge Knight book cover"
+        },
+        "title":"The Hedge Knight",
+        "author": "George R.R. Martin",
+        "rating": "5",
+        "status": "Currently Reading",
+        "synopsis": "The Hedge Knight follows Dunk, a squire who becomes a hedge knight after his master dies, as he travels to a tourney at Ashford, taking on a young boy named Egg as his squire, and finds himself entangled in court intrigue, a stolen dragon egg, and a dangerous plot involving a potential Targaryen claimant, forcing him into a trial by combat where he must find allies to fight for him. "
+    }
+]
