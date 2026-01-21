e2557a1a4dc38c7e3e66c73bfa1e7637b9cfc5e9
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2026-01-21 21:41:43 +0000

    Create book rating variable using State

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,4 +1,18 @@
+import React from "react"
+
 export default function Entry(props){
+    // Create book rating variable using State
+    const [rating, setRating] = React.useState(Number(props.rating));
+
+    function add(){
+        if(rating < 10)
+            setRating(prev => prev + 1) 
+    }
+
+    function subtract(){
+        if(rating > 0)
+            setRating(prev => prev - 1)
+    }
 
     return (
         <>
@@ -8,7 +22,10 @@ export default function Entry(props){
             <div>
                 <h1>{props.title}</h1>
                 <p><strong>Author:</strong> {props.author}</p>
-                <p><strong>Rating:</strong> {props.rating}/10</p>
+                <p>
+                    <strong>Rating:</strong> {rating}/10 <button onClick={add}>+</button>
+                    <button onClick={subtract}>-</button>
+                </p>
                 <p><strong>Status:</strong> {props.status}</p>
                 <p><strong>Synopsis:</strong> {props.synopsis}</p>
             </div>
