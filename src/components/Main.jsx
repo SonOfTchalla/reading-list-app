f7dbf3e9557016b45740b37833cb7d42bca4c2d0
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2026-01-24 00:49:45 +0000

    Add state for BGColor and make form fields required

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,14 +18,22 @@ export default function Main(){
 
     const [addingBook, setAddingBook] = React.useState(false)
     const [btnText, setBtnText] = React.useState("Add New Book")
+    const [bgColor, setbgColor] = React.useState({backgroundColor:"orange"})
     
     function toggle(){
         setAddingBook(!addingBook)
         setBtnText((prev)=>{
-            if(prev === "Back"){
+            if(prev === "⬅ Back"){
                 return "Add New Book"
             } else{
-                return "Back"
+                return "⬅ Back"
+            }
+        })
+        setbgColor((prev)=>{
+            if(prev.backgroundColor === "orange"){
+                return {backgroundColor:"red"}
+            } else{
+                return {backgroundColor:"orange"}
             }
         })
     }
@@ -33,26 +41,26 @@ export default function Main(){
     return (
         <>
         {!addingBook && entryData}
-        <AddBook handleClick={toggle} text={btnText} />
+        <AddBook handleClick={toggle} text={btnText} BGColor={bgColor}/>
 
         {addingBook &&
         <form action="">
             <h1>Add a new book</h1>
             <label htmlFor="image">
                 Image URL: 
-                <input placeholder="https://example.com/img.png" id="name" name="image" type="text" />
+                <input placeholder="https://example.com/img.png" id="name" name="image" type="text" required/>
             </label>
             <label htmlFor="title">
                 Title: 
-                <input placeholder="e.g. A Game of Thrones" id="title" name="title" type="text" />
+                <input placeholder="e.g. A Game of Thrones" id="title" name="title" type="text" required/>
             </label>
             <label htmlFor="author">
                 Author: 
-                <input placeholder="e.g. HG Wells" id="author" name="author" type="text" />
+                <input placeholder="e.g. HG Wells" id="author" name="author" type="text" required/>
             </label>
             <label htmlFor="status">
                 Status: 
-                <select defaultValue="">
+                <select defaultValue="" required>
                     <option value="" disabled="disabled">---Select a book status---</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="dropped">Dropped</option>
@@ -61,7 +69,7 @@ export default function Main(){
             </label>
             <label htmlFor="synopsis">
                 Synopsis: 
-                <textarea placeholder="Provide a blurb for the book" id="synopsis" name="synopsis" type="text" />
+                <textarea placeholder="Provide a blurb for the book" id="synopsis" name="synopsis" type="text" required/>
             </label>
             <button id="submitBtn" type="submit">Submit</button>
         </form>
