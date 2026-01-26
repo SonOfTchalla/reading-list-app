e063c64ad1107071295567e21fa164bb4addfd5c
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2026-01-26 23:45:52 +0000

    Add state to entry data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,12 @@ import Entry from "./Entry"
 import sampleData from "../sampleData"
 import AddBook from "./AddBook"
 import React from "react"
+import NewBook from "./NewBook"
 
 export default function Main(){
-    const entryData = sampleData.map((entry)=>(
+    const [entries, addEntry] = React.useState(sampleData);
+    
+    const entryData = entries.map((entry)=>(
      <Entry 
         key={entry.id}
         img={entry.img}
@@ -43,36 +46,8 @@ export default function Main(){
         {!addingBook && entryData}
         <AddBook handleClick={toggle} text={btnText} BGColor={bgColor}/>
 
-        {addingBook &&
-        <form action="">
-            <h1>Add a new book</h1>
-            <label htmlFor="image">
-                Image URL: 
-                <input placeholder="https://example.com/img.png" id="name" name="image" type="text" required/>
-            </label>
-            <label htmlFor="title">
-                Title: 
-                <input placeholder="e.g. A Game of Thrones" id="title" name="title" type="text" required/>
-            </label>
-            <label htmlFor="author">
-                Author: 
-                <input placeholder="e.g. HG Wells" id="author" name="author" type="text" required/>
-            </label>
-            <label htmlFor="status">
-                Status: 
-                <select defaultValue="" required>
-                    <option value="" disabled="disabled">---Select a book status---</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="dropped">Dropped</option>
-                    <option value="completed">Completed</option>
-                </select>
-            </label>
-            <label htmlFor="synopsis">
-                Synopsis: 
-                <textarea placeholder="Provide a blurb for the book" id="synopsis" name="synopsis" type="text" required/>
-            </label>
-            <button id="submitBtn" type="submit">Submit</button>
-        </form>
+        {addingBook && 
+            <NewBook addEntry={addEntry} toggle={toggle} />
         }
         </>
     )
