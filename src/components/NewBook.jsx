1813ac38a6207316f16320aafe2347f7b230680f
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2026-01-28 22:58:51 +0000

    connect app to OpenLibrary API to retrie book info using title

--- src/components/NewBook.jsx
+++ src/components/NewBook.jsx
@@ -1,5 +1,32 @@
 export default function NewBook({addEntry, toggle, entries}){
     function addNewBook(formData){
+        let title = formData.get("title")
+        title = title.split(" ").join("+").toLowerCase()
+        console.log(title)
+        fetch(`https://openlibrary.org/search.json?q=${title}`)
+            .then(res => res.json())
+            .then(function(data){
+                addEntry((prev) => 
+                    [
+                        ...prev,
+                        {
+                            "id":entries[entries.length - 1].id + 1,
+                            "img": {
+                                "src": `https://covers.openlibrary.org/b/id/${data.docs[0].cover_i}-M.jpg`,
+                                "alt": formData.get("imageAlt")
+                            },
+                            "title": data.docs[0].title,
+                            "author": data.docs[0].author_name[0],
+                            "rating": 0,
+                            "status": formData.get("bookStatus"),
+                            "synopsis": "[INSERT SYNOPSIS HERE]"
+                        }
+                    ]
+                )
+                console.log(entries)
+                toggle()
+            })
+            /*
         addEntry((prev) => 
             [
                 ...prev,
@@ -9,7 +36,7 @@ export default function NewBook({addEntry, toggle, entries}){
                         "src": formData.get("imageSrc"),
                         "alt": formData.get("imageAlt")
                     },
-                    "title":formData.get("title"),
+                    "title":title,
                     "author": formData.get("author"),
                     "rating": 0,
                     "status": formData.get("bookStatus"),
@@ -19,27 +46,22 @@ export default function NewBook({addEntry, toggle, entries}){
         )
         console.log(entries)
         toggle()
+        */
     }
     
     return (
         <form action={addNewBook}>
             <h1>Add a new book</h1>
-            <label htmlFor="imageSrc">
-                Image URL: 
-                <input placeholder="https://example.com/img.png" id="imageSrc" name="imageSrc" type="text" required/>
-            </label>
-            <label htmlFor="imageAlt">
-                Image Alt: 
-                <input placeholder="https://example.com/img.png" id="imageAlt" name="imageAlt" type="text" required/>
-            </label>
             <label htmlFor="title">
                 Title: 
                 <input placeholder="e.g. A Game of Thrones" id="title" name="title" type="text" required/>
             </label>
+            {/*
             <label htmlFor="author">
                 Author: 
                 <input placeholder="e.g. HG Wells" id="author" name="author" type="text" required/>
             </label>
+            */}
             <label htmlFor="status">
                 Status: 
                 <select id="status"
@@ -51,10 +73,12 @@ export default function NewBook({addEntry, toggle, entries}){
                     <option value="Completed">Completed</option>
                 </select>
             </label>
+            {/*
             <label htmlFor="synopsis">
                 Synopsis: 
                 <textarea placeholder="Provide a blurb for the book" id="synopsis" name="synopsis" type="text" required/>
             </label>
+            */}
             <button id="submitBtn" type="submit">Submit</button>
         </form>
     )
